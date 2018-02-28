2259123e7a4af4214e8c5a3e0427a14fa0f0d7d8
Ananya Agrawal <33188930+ananya-agrawal@users.noreply.github.com>
2018-02-28 23:10:25 +0530

    Update events.js

--- static/js/events.js
+++ static/js/events.js
@@ -5,8 +5,10 @@ const app = new Vue({
 	sortedEvents : [],
 	imgUrl : ["static/img/event1.jpg", "static/img/event2.jpg", "static/img/event3.jpg", "static/img/event4.jpg","static/img/event5.jpg","static/img/event6.jpg"]
     },
+	https://girls-do-code.github.io/
+	https://opencode18.github.io/OpenCode-Collaborative/
     created() {
-        fetch('https://opencode18.github.io/Girls-who-code/static/js/events.json')
+        fetch('https://girls-do-code.github.io/static/js/events.json')
             .then(response => response.json())
             .then(json => {
                 this.events = json.events
